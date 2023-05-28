e9cc3ba2b403cecf5bef9336d31240e1e897c561
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-05-28 12:19:03 +0000

    phpcs autofixes

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -8,12 +8,12 @@ use phpOMS\Router\RouteVerb;
 return [
     '^.*/controlling/budget/dashboard.*$' => [
         [
-            'dest' => '\Modules\BudgetManagement\Controller\BackendController:viewBudgetingDashboard',
-            'verb' => RouteVerb::GET,
+            'dest'       => '\Modules\BudgetManagement\Controller\BackendController:viewBudgetingDashboard',
+            'verb'       => RouteVerb::GET,
             'permission' => [
                 'module' => BackendController::MODULE_NAME,
-                'type'  => PermissionType::READ,
-                'state' => PermissionState::BUDGET,
+                'type'   => PermissionType::READ,
+                'state'  => PermissionState::BUDGET,
             ],
         ],
     ],

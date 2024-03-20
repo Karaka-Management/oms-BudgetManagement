d09735a2b7e41a4cbaaabe162ea65d45b753e449
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-03-20 03:00:24 +0000

    fix tests

---
+++ Controller/ApiController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   Modules\BudgetManagement
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\BudgetManagement\Controller;
+
+/**
+ * BudgetManagement api controller class.
+ *
+ * @package Modules\BudgetManagement
+ * @license OMS License 2.0
+ * @link    https://jingga.app
+ * @since   1.0.0
+ */
+final class ApiController extends Controller
+{
+}
